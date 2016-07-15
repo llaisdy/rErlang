bf3a84c40b7159490937c32fff2e0ea408acf3cd
Ivan Uemlianin <ivan@llaisdy.com>
2016-07-15 07:35:48 +0000

    non-functional whitespace only

--- src/eri.erl
+++ src/eri.erl
@@ -1,21 +1,29 @@
 -module(eri).
 
-% API
--export([start/0, start/1, stop/0,init/1]).
--export([connect/0, parse/1, eval/1, sum/2]).
-
+-export([start/0, start/1, connect/0, eval/1, stop/0]).
+-export([init/1, parse/1, sum/2]).
 
 start()->	
-	Path = case os:cmd("echo $SENSOR_PATH") of
-			"\n" -> 
-				".";
-			SensorPath ->
-				SensorPath--"\n"
-		  end,
-		  erlang:display(Path),
+    Path = case os:cmd("echo $SENSOR_PATH") of
+	       "\n" -> 
+		   ".";
+	       SensorPath ->
+		   SensorPath--"\n"
+	   end,
+    erlang:display(Path),
     start(Path++"/lib/rErlang/bin/ERI-0.1").
+
 start(ExtPrg) ->
     register(?MODULE, spawn_link(?MODULE, init, [ExtPrg])).
+
+connect() -> 
+    call_port({setup}).
+
+eval(X)->    
+    case parse(X) of
+	{ok,Result} -> call_port({eval,Result});
+	_ -> {error}
+    end.
 
 stop() ->
     case call_port({stop}) of 
@@ -29,29 +37,23 @@ stop() ->
 	    ?MODULE ! stop
     end.
 
-connect() -> 
-    call_port({setup}).
-
-parse(X)->
-    call_port({parse,X}).
-
-    
-eval(X)->    
-    case parse(X) of
-	{ok,Result} -> call_port({eval,Result});
-	_ -> {error}
-    end.
-
 init(ExtPrg)->
     process_flag(trap_exit, true),
     Port = open_port({spawn, ExtPrg}, [{packet,2}, binary]),
     loop(Port).
 
-sum(X,Y) -> call_port({sum, X, Y}).
+parse(X)->
+    call_port({parse,X}).
+
+sum(X,Y) ->
+    call_port({sum, X, Y}).
+
+
+%%%% private
 
 call_port(Msg) ->
-    %erlang:display("sending message: "),
-    %erlang:display(Msg),
+    %% erlang:display("sending message: "),
+    %% erlang:display(Msg),
     ?MODULE ! {call, self(), Msg},
     receive
 	{?MODULE, Result}->
@@ -64,7 +66,7 @@ loop(Port) ->
 	    Port ! {self(), {command, term_to_binary(Msg)}},
 	    receive
 		{Port, {data, Data}} ->
-		    %erlang:display(Data),
+		    %% erlang:display(Data),
 		    Caller ! {?MODULE, binary_to_term(Data)}
 	    end,
 	    loop(Port);
