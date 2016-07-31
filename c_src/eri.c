ea00e6002971d51f8bee402882cd83f88e43f5d8
Ivan Uemlianin <ivan@llaisdy.com>
2016-07-31 09:16:08 +0000

    whitespace only

--- c_src/eri.c
+++ c_src/eri.c
@@ -19,15 +19,14 @@ long parse(const char *s){
 
 long erl_eval(long exp){
   long e;  
-  int er=0;
+  int er = 0;
  
-  e = r_eval(exp,&er);  
+  e = r_eval(exp, &er);
 
   return e;
 }
 
 int convert(long exp, ei_x_buff *result){
-
   SEXP es;
   int type, i;
   double *vd;
@@ -36,8 +35,8 @@ int convert(long exp, ei_x_buff *result){
   es = L2SEXP(exp);
   type = TYPEOF(es);
   unsigned int len = LENGTH(es);
-  //fprintf(stderr,"TYPEOF:%d\n",type);
-  //fprintf(stderr,"LENGTH:%d\n",len);  
+  //fprintf(stderr,"TYPEOF: %d\n",type);
+  //fprintf(stderr,"LENGTH: %d\n",len);
 
   if(type == REALSXP){    
     if(len > 0){   
@@ -68,14 +67,14 @@ int convert(long exp, ei_x_buff *result){
       }
     }   
   }else if(type == VECSXP){
-    
-    if(ei_x_encode_atom(result,"ok") || ei_x_encode_atom(result,"VECSXP") || ei_x_encode_long(result,exp)){
+    if(ei_x_encode_atom(result,"ok") || ei_x_encode_atom(result,"VECSXP") ||
+       ei_x_encode_long(result,exp)){
     }
   }else if(type == STRSXP){
     if(len>0){
       if(ei_x_encode_atom(result,"ok") || ei_x_encode_atom(result,"STRSXP") || 
 	 ei_x_encode_list_header(result,len)){
-      }	
+      }
       i = 0;
       while(i < len){	
 	ei_x_encode_string(result,CHAR(STRING_ELT(es,i)));
@@ -85,9 +84,9 @@ int convert(long exp, ei_x_buff *result){
       }
     }    
   }else{
-    if(ei_x_encode_atom(result,"ok") || ei_x_encode_atom(result,"OTHER") || ei_x_encode_long(result,exp)){
-    } 
+    if(ei_x_encode_atom(result,"ok") || ei_x_encode_atom(result,"OTHER") ||
+       ei_x_encode_long(result,exp)){
+    }
   }  
-
   return 0;
 }
