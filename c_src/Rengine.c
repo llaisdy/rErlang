88b57311db68cd4cd80bbff926f9ada20237b357
Ivan Uemlianin <ivan@llaisdy.com>
2016-07-31 08:56:36 +0000

    whitespace only

--- c_src/Rengine.c
+++ c_src/Rengine.c
@@ -12,10 +12,8 @@ static char *Rargv[] = {"rerlang", "--no-save", "--no-restore","--quiet", NULL};
 
 
 int r_setup(){
-
   int initRes;
   initRes=initR(N_R_ARGS, Rargv);
-  
   return initRes;
 }
 
@@ -24,42 +22,31 @@ void r_run(){
 }
 
 long r_parse(const char *s){
-
   ParseStatus ps;
   SEXP pstr, cv;
 
   PROTECT(cv=allocVector(STRSXP,1));
   SET_STRING_ELT(cv, 0, mkChar(s));
   UNPROTECT(1);
-  //fprintf(stderr,"parsing \"%s\"\n", CHAR(STRING_ELT(cv,0)));    
   pstr=R_ParseVector(cv, 1, &ps, R_NilValue);  
-
-  //fprintf(stderr,"%d\n",TYPEOF(pstr));  
-  //fprintf(stderr,"parse status=%d, result=%x, type=%d\n", ps, (int) pstr, (pstr!=0)?TYPEOF(pstr):0);
-  
   return SEXP2L(pstr); 
-
 }
 
 long r_eval(long exp, int *er){  
-
   SEXP es, exps=L2SEXP(exp);
   int i=0, l; 
 
-  
-  if (TYPEOF(exps)==EXPRSXP) { /* if the object is a list of exps, eval them one by one */
-    l=LENGTH(exps);
-    while (i<l) {
-      es=R_tryEval(VECTOR_ELT(exps,i), R_GlobalEnv, er);
+  if (TYPEOF(exps) == EXPRSXP) { /* if the object is a list of exps, eval them one by one */
+    l = LENGTH(exps);
+    while (i < l) {
+      es = R_tryEval(VECTOR_ELT(exps, i), R_GlobalEnv, er);
       i++;
     }
-  } else
-    es=R_tryEval(exps, R_GlobalEnv, er); 
-
+  } else {
+    es = R_tryEval(exps, R_GlobalEnv, er); 
+  }
   return SEXP2L(es);
-
 }
-
 
 int r_assign(){
   return 0;
